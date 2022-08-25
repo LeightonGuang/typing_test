88e9f5014dd0cbe5158fc5ee48b8b04ab5377b19
Leighton417 <leighton.guang@gmail.com>
2022-08-25 22:56:08 +0800

    generates random words that fills with overflow

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
+let randomWords = ["the", "at", "there", "some", "my", "of", "be", "use", "her", "than", 
+                  "and", "this", "an", "would", "first", "a", "have", "each", "make", 
+                  "water", "to", "from", "which", "like", "been", "in", "or", "she", 
+                  "him", "call", "is", "one", "do", "into", "who", "you", "had", "how",	
+                  "time", "oil", "that", "by", "their", "has", "its", "it", "word", "if", 
+                  "look", "now", "he", "but", "will", "two", "find", "was", 
+                  "not", "up", "more", "long", "for", "what", "other", "write", "down",
+                  "on", "all", "about", "go", "day", "are", "were", "out", "see", "did",
+                  "as", "we", "many", "number", "get", "with", "when", "then", "no", 
+                  "come", "his", "your", "them", "way", "made", "they", "can", "these",
+                  "could", 'may', "I", "said", "so", "people", "part"];
 let history = [];
-
+let subHistory = [];
 
 //=====================================================
-let typing = document.querySelector("#typingID");
+let typeField = document.querySelector("#typingID");
 let typedDisplay = document.querySelector("#typedDisplayID");
+let p1 = document.querySelector("#p1");
+let p2 = document.querySelector("#p2");
+let p3 = document.querySelector("#p3");
 
-typing.addEventListener("keydown", (e) => {
+typeField.addEventListener("keydown", (e) => {
+  //if spacebar is pressed
   if(e.code == "Space"){
     //removes the space in front
     e.preventDefault();
-    history.push(typing.value + " ");
-    typedDisplay.innerHTML += (" " + typing.value);
-    typing.value = "";
+    subHistory.push(typeField.value + " ");
+    p3.innerHTML += (" " + typeField.value);
+    typeField.value = "";
     console.log(history);
   }
 
-  if(typing.value == "" && e.code == "Backspace"){
+  //if backspace is pressed
+  if(typeField.value == "" && e.code == "Backspace"){
     if(history.length == 0){
       console.log(history);
     }else{
       console.log("backspace");
-      typing.value = history[history.length - 1];
+      typeField.value = history[history.length - 1];
       typedDisplay.innerHTML = typedDisplay.innerHTML.substring(0, typedDisplay.innerHTML.lastIndexOf(" "));
       history.pop();
       console.log(history);
@@ -33,18 +49,44 @@ typing.addEventListener("keydown", (e) => {
 function restart(){
   history = [];
   console.log(history);
-  typing.value = "";
-  typedDisplay.innerHTML = "";
+  typeField.value = "";
+  p1.innerHTML = " ";
+  p2.innerHTML = " ";
+  p3.innerHTML = " ";
 }
 
-let randomWords = ["the", "at", "there", "some", "my", "of", "be", "use", "her", "than", 
-                  "and", "this", "an", "would", "first", "a", "have", "each", "make", 
-                  "water", "to", "from", "which", "like", "been", "in", "or", "she", 
-                  "him", "call", "is", "one", "do", "into", "who", "you", "had", "how",	
-                  "time", "oil", "that", "by", "their", "has", "its", "it", "word", "if", 
-                  "look", "now", "he", "but", "will", "two", "find", "was", 
-                  "not", "up", "more", "long", "for", "what", "other", "write", "down",
-                  "on", "all", "about", "go", "day", "are", "were", "out", "see", "did",
-                  "as", "we", "many", "number", "get", "with", "when", "then", "no", 
-                  "come", "his", "your", "them", "way", "made", "they", "can", "these",
-                  "could", 'may', "I", "said", "so", "people", "part"];
+//=====================================================
+
+function checkOverflow(el)
+{
+   let curOverflow = el.style.overflow;
+
+   if ( !curOverflow || curOverflow === "visible" )
+      el.style.overflow = "hidden";
+
+   let isOverflowing = el.clientWidth < el.scrollWidth 
+      || el.clientHeight < el.scrollHeight;
+
+   el.style.overflow = curOverflow;
+
+   return isOverflowing;
+}
+
+//=====================================================
+
+function generateWords(){
+  p1.innerHTML = p2.innerHTML;
+  p2.innerHTML = p3.innerHTML;
+  p3.innerHTML = "new";
+  while(checkOverflow(typedDisplay) == false){
+    let randomIndex = Math.floor(Math.random() * randomWords.length);
+    subHistory.push(randomWords[randomIndex]);
+    p3.innerHTML = subHistory.join(" ");
+  }
+  //remove last element and put to new <p>
+  let temp = subHistory.pop();
+  p3.innerHTML = subHistory.join(" ");
+  history.push(subHistory);
+  subHistory = [temp];
+  console.log(history[history.length - 1]);
+}
