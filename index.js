29e06a11149b58bdabcccf57a3fe68d79f64ca01
Leighton417 <leighton.guang@gmail.com>
2022-08-26 04:03:01 +0800

    working generate word

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-let randomWords = ["the", "at", "there", "some", "my", "of", "be", "use", "her", "than", 
-                  "and", "this", "an", "would", "first", "a", "have", "each", "make", 
-                  "water", "to", "from", "which", "like", "been", "in", "or", "she", 
-                  "him", "call", "is", "one", "do", "into", "who", "you", "had", "how",	
-                  "time", "oil", "that", "by", "their", "has", "its", "it", "word", "if", 
-                  "look", "now", "he", "but", "will", "two", "find", "was", 
-                  "not", "up", "more", "long", "for", "what", "other", "write", "down",
-                  "on", "all", "about", "go", "day", "are", "were", "out", "see", "did",
-                  "as", "we", "many", "number", "get", "with", "when", "then", "no", 
-                  "come", "his", "your", "them", "way", "made", "they", "can", "these",
-                  "could", 'may', "I", "said", "so", "people", "part"];
+let randomWords = [
+  "the", "at", "there", "some", "my", "of", "be", "use", "her", "than", 
+  "and", "this", "an", "would", "first", "a", "have", "each", "make", 
+  "water", "to", "from", "which", "like", "been", "in", "or", "she", 
+  "him", "call", "is", "one", "do", "into", "who", "you", "had", "how",	
+  "time", "oil", "that", "by", "their", "has", "its", "it", "word", "if", 
+  "look", "now", "he", "but", "will", "two", "find", "was", 
+  "not", "up", "more", "long", "for", "what", "other", "write", "down",
+  "on", "all", "about", "go", "day", "are", "were", "out", "see", "did",
+  "as", "we", "many", "number", "get", "with", "when", "then", "no", 
+  "come", "his", "your", "them", "way", "made", "they", "can", "these",
+  "could", 'may', "I", "said", "so", "people", "part"];
+
+let onIndex = 0;
 let history = [];
 let subHistory = [];
+
+let typedHistory = [];
+let typedSubHistory = [];
 
 //=====================================================
 let typeField = document.querySelector("#typingID");
@@ -24,16 +30,22 @@ typeField.addEventListener("keydown", (e) => {
   if(e.code == "Space"){
     //removes the space in front
     e.preventDefault();
-    subHistory.push(typeField.value + " ");
+    typedSubHistory.push(typeField.value + " ");
     p3.innerHTML += (" " + typeField.value);
     typeField.value = "";
-    console.log(history);
+    console.log(typedHistory[typedHistory.length - 1]);
+
+    if(typedSubHistory.length == subHistory.length){
+      typedHistory.push(typedSubHistory);
+      typedSubHistory = [];
+    }
+    console.log(typedHistory[typedHistory.length - 1]);
   }
 
   //if backspace is pressed
   if(typeField.value == "" && e.code == "Backspace"){
-    if(history.length == 0){
-      console.log(history);
+    if(typedSubHistory.length == 0){
+      console.log(typedHistory);
     }else{
       console.log("backspace");
       typeField.value = history[history.length - 1];
@@ -90,3 +102,30 @@ function generateWords(){
   subHistory = [temp];
   console.log(history[history.length - 1]);
 }
+
+//=====================================================
+
+function checkCorrect(){
+  if(subHistory[subHistory.length - 1] == typedSubHistory[typedSubHistory.length - 1]){
+    correctHistory.push(true);
+  }
+}
+
+//=====================================================
+
+function addSpan(){
+  let randomIndex = Math.floor(Math.random() * randomWords.length);
+  p1.innerHTML = p2.innerHTML;
+  p2.innerHTML = p3.innerHTML;
+  subHistory.push(`<span>${randomWords[randomIndex]}</span>`);
+  p3.innerHTML = subHistory;
+}
+
+//=====================================================
+
+function start(){
+  generateWords();
+  generateWords();
+}
+
+start();
