ee7d707b64f4fab7e4fcaf4c472e6538455ca280
Leighton Guang <leighton.guang@gmail.com>
2023-11-20 11:23:49 +0000

    style: run prettier

--- index.js
+++ index.js
@@ -1,15 +1,104 @@
 let randomWords = [
-  "the", "at", "there", "some", "my", "of", "be", "use", "her", "than", 
-  "and", "this", "an", "would", "first", "a", "have", "each", "make", 
-  "water", "to", "from", "which", "like", "been", "in", "or", "she", 
-  "him", "call", "is", "one", "do", "into", "who", "you", "had", "how",	
-  "time", "oil", "that", "by", "their", "has", "its", "it", "word", "if", 
-  "look", "now", "he", "but", "will", "two", "find", "was", 
-  "not", "up", "more", "long", "for", "what", "other", "write", "down",
-  "on", "all", "about", "go", "day", "are", "were", "out", "see", "did",
-  "as", "we", "many", "number", "get", "with", "when", "then", "no", 
-  "come", "his", "your", "them", "way", "made", "they", "can", "these",
-  "could", 'may', "I", "said", "so", "people", "part"
+  "the",
+  "at",
+  "there",
+  "some",
+  "my",
+  "of",
+  "be",
+  "use",
+  "her",
+  "than",
+  "and",
+  "this",
+  "an",
+  "would",
+  "first",
+  "a",
+  "have",
+  "each",
+  "make",
+  "water",
+  "to",
+  "from",
+  "which",
+  "like",
+  "been",
+  "in",
+  "or",
+  "she",
+  "him",
+  "call",
+  "is",
+  "one",
+  "do",
+  "into",
+  "who",
+  "you",
+  "had",
+  "how",
+  "time",
+  "oil",
+  "that",
+  "by",
+  "their",
+  "has",
+  "its",
+  "it",
+  "word",
+  "if",
+  "look",
+  "now",
+  "he",
+  "but",
+  "will",
+  "two",
+  "find",
+  "was",
+  "not",
+  "up",
+  "more",
+  "long",
+  "for",
+  "what",
+  "other",
+  "write",
+  "down",
+  "on",
+  "all",
+  "about",
+  "go",
+  "day",
+  "are",
+  "were",
+  "out",
+  "see",
+  "did",
+  "as",
+  "we",
+  "many",
+  "number",
+  "get",
+  "with",
+  "when",
+  "then",
+  "no",
+  "come",
+  "his",
+  "your",
+  "them",
+  "way",
+  "made",
+  "they",
+  "can",
+  "these",
+  "could",
+  "may",
+  "I",
+  "said",
+  "so",
+  "people",
+  "part",
 ];
 
 let startTime = false;
@@ -54,41 +143,42 @@ typeField.focus();
 
 typeField.addEventListener("keydown", (e) => {
   //if spacebar is pressed
-  if(e.code == "Space"){
+  if (e.code == "Space") {
     //if there are words in type field when space is pressed
-    if(typeField.value != ""){
+    if (typeField.value != "") {
       onArray = wordsArray[wordsArrayIndex];
       console.log("space");
       //removes the space
       e.preventDefault();
       typedSubArray.push(typeField.value + " ");
       replaceWord(onArray, typeField.value);
-      console.log(`onTypedSubArrayIndex: ${onTypedSubArrayIndex} \ttypedArray length: ${typedArray.length} \ntypedSubArray: ${typedSubArray}`);
+      console.log(
+        `onTypedSubArrayIndex: ${onTypedSubArrayIndex} \ttypedArray length: ${typedArray.length} \ntypedSubArray: ${typedSubArray}`
+      );
       console.log(`wordsArrayIndex: ${wordsArrayIndex} \nonArray: ${onArray}`);
       p2.innerHTML = onArray.join(" ");
       onTypedSubArrayIndex++;
       typeField.value = "";
 
-    //if there are no words
-    }else if (typeField.value == ""){
+      //if there are no words
+    } else if (typeField.value == "") {
       //removes the space
       e.preventDefault();
       typeField.value = "";
       console.log("nothing to delete");
     }
-    
 
     //if reached the end of the array, reset index
-    if(wordsArray[wordsArrayIndex].length == typedSubArray.length){
+    if (wordsArray[wordsArrayIndex].length == typedSubArray.length) {
       console.log(typedSubArray);
       typedArray.push(typedSubArray);
       typedSubArray = [];
       onTypedSubArrayIndex = 0;
       wordsArrayIndex++;
       //if there already words created before then skip generate
-      if(wordsArrayIndex == wordsArray.length - 1){
+      if (wordsArrayIndex == wordsArray.length - 1) {
         generateWords();
-      }else{
+      } else {
         p1.innerHTML = wordsArray[wordsArrayIndex - 1].join(" ");
         p2.innerHTML = wordsArray[wordsArrayIndex].join(" ");
         p3.innerHTML = wordsArray[wordsArrayIndex + 1].join(" ");
@@ -99,69 +189,84 @@ typeField.addEventListener("keydown", (e) => {
   }
 
   //*Fix* if backspace is pressed
-  if(typeField.value == "" && e.code == "Backspace"){
-    
+  if (typeField.value == "" && e.code == "Backspace") {
     //if at the start of the array
-    if(onTypedSubArrayIndex == 0 && typedArray.length == 0){
+    if (onTypedSubArrayIndex == 0 && typedArray.length == 0) {
       console.log("nothing left to delete");
-    
-    //if it reached the front of typedsubArray but there are still arrays in front
-    }else if(onTypedSubArrayIndex == 0 && typedArray.length != 0){
+
+      //if it reached the front of typedsubArray but there are still arrays in front
+    } else if (onTypedSubArrayIndex == 0 && typedArray.length != 0) {
       wordsArrayIndex--;
       onArray = wordsArray[wordsArrayIndex];
       //if it reaches the top of the words array
-      console.log("scroll up");  
+      console.log("scroll up");
       console.log("replace typedSubArray with the one before");
       typedSubArray = typedArray[typedArray.length - 1];
       console.log("remove array from typed Array");
       typedArray.pop();
       onTypedSubArrayIndex = typedSubArray.length - 1;
-      wordsArray[wordsArrayIndex][onTypedSubArrayIndex] = extract(wordsArray[wordsArrayIndex][onTypedSubArrayIndex]);
+      wordsArray[wordsArrayIndex][onTypedSubArrayIndex] = extract(
+        wordsArray[wordsArrayIndex][onTypedSubArrayIndex]
+      );
       //removing the space at the end
-      if(wordsArray[wordsArrayIndex][onTypedSubArrayIndex] == typedSubArray[onTypedSubArrayIndex].slice(0, -1)){
+      if (
+        wordsArray[wordsArrayIndex][onTypedSubArrayIndex] ==
+        typedSubArray[onTypedSubArrayIndex].slice(0, -1)
+      ) {
         letters -= wordsArray[wordsArrayIndex][onTypedSubArrayIndex].length;
       }
-        
-      if((wordsArrayIndex - 1 < 0)){
+
+      if (wordsArrayIndex - 1 < 0) {
         p1.innerHTML = " ";
-      }else{
+      } else {
         p1.innerHTML = wordsArray[wordsArrayIndex - 1].join(" ");
       }
       p2.innerHTML = wordsArray[wordsArrayIndex].join(" ");
       p3.innerHTML = wordsArray[wordsArrayIndex + 1].join(" ");
-      console.log(`typedArray: ${typedArray} \ntypedSubArray: ${typedSubArray}`);
-      console.log(`onArray: ${onArray}`)
+      console.log(
+        `typedArray: ${typedArray} \ntypedSubArray: ${typedSubArray}`
+      );
+      console.log(`onArray: ${onArray}`);
       typeField.value = typedSubArray[typedSubArray.length - 1];
       typedSubArray.pop();
-    
-    //going back 1 word
-    }else{
+
+      //going back 1 word
+    } else {
       onTypedSubArrayIndex--;
       console.log("remove color from words");
-      wordsArray[wordsArrayIndex][onTypedSubArrayIndex] = extract(wordsArray[wordsArrayIndex][onTypedSubArrayIndex]);
+      wordsArray[wordsArrayIndex][onTypedSubArrayIndex] = extract(
+        wordsArray[wordsArrayIndex][onTypedSubArrayIndex]
+      );
       //removing the space at the end
-      if(wordsArray[wordsArrayIndex][onTypedSubArrayIndex] == typedSubArray[onTypedSubArrayIndex].slice(0, -1)){
+      if (
+        wordsArray[wordsArrayIndex][onTypedSubArrayIndex] ==
+        typedSubArray[onTypedSubArrayIndex].slice(0, -1)
+      ) {
         letters -= wordsArray[wordsArrayIndex][onTypedSubArrayIndex].length;
       }
       p2.innerHTML = onArray.join(" ");
       typeField.value = typedSubArray[typedSubArray.length - 1];
       console.log(`typedSubArray: ${typedSubArray}`);
-      console.log(`onArray: ${onArray}`)
+      console.log(`onArray: ${onArray}`);
       typedSubArray.pop();
     }
   }
-  if(typedArray.length > 0 || typedSubArray.length > 0 || typeField.value != ""){
+  if (
+    typedArray.length > 0 ||
+    typedSubArray.length > 0 ||
+    typeField.value != ""
+  ) {
     startTime = true;
   }
 
-  
-  wpm_calc = parseFloat((letters - letter_penalty) / 5 / ((calc_seconds - seconds) / 60)); 
-  
+  wpm_calc = parseFloat(
+    (letters - letter_penalty) / 5 / ((calc_seconds - seconds) / 60)
+  );
+
   //if its negative ir infinity number
-  if(wpm_calc <= 0 || !(isFinite(wpm_calc))){
+  if (wpm_calc <= 0 || !isFinite(wpm_calc)) {
     wpm.innerHTML = (0).toFixed(2);
-  
-  }else{
+  } else {
     console.log("not infinity: " + wpm_calc);
     wpm.innerHTML = wpm_calc.toFixed(2);
   }
@@ -170,7 +275,7 @@ typeField.addEventListener("keydown", (e) => {
 
 //=====================================================
 
-function restart(){
+function restart() {
   typeField.removeAttribute("disabled");
   letters = 0;
   letter_penalty = 0;
@@ -195,33 +300,31 @@ function restart(){
 
 //=====================================================
 
-function checkOverflow(el)
-{
-   let curOverflow = el.style.overflow;
+function checkOverflow(el) {
+  let curOverflow = el.style.overflow;
 
-   if ( !curOverflow || curOverflow === "visible" )
-      el.style.overflow = "hidden";
+  if (!curOverflow || curOverflow === "visible") el.style.overflow = "hidden";
 
-   let isOverflowing = el.clientWidth < el.scrollWidth 
-      || el.clientHeight < el.scrollHeight;
+  let isOverflowing =
+    el.clientWidth < el.scrollWidth || el.clientHeight < el.scrollHeight;
 
-   el.style.overflow = curOverflow;
+  el.style.overflow = curOverflow;
 
-   return isOverflowing;
+  return isOverflowing;
 }
 
 //=====================================================
 
-function generateWords(){
+function generateWords() {
   let tempArr = [];
   console.log("scroll down");
   p1.innerHTML = p2.innerHTML;
   p2.innerHTML = p3.innerHTML;
   p3.innerHTML = "new";
-  while(checkOverflow(typedDisplay) == false){
+  while (checkOverflow(typedDisplay) == false) {
     let randomIndex = Math.floor(Math.random() * randomWords.length);
     //if there are two same words in a row
-    while(tempArr[tempArr.length - 1] == randomWords[randomIndex]){
+    while (tempArr[tempArr.length - 1] == randomWords[randomIndex]) {
       randomIndex = Math.floor(Math.random() * randomWords.length);
     }
     tempArr.push(randomWords[randomIndex]);
@@ -237,35 +340,36 @@ function generateWords(){
 
 //=====================================================
 
-function replaceWord(wordArray, typed){
-  if(wordArray[onTypedSubArrayIndex] == typed){
+function replaceWord(wordArray, typed) {
+  if (wordArray[onTypedSubArrayIndex] == typed) {
     letters += wordsArray[wordsArrayIndex][onTypedSubArrayIndex].length;
     console.log("replace word green");
-    wordArray.splice(onTypedSubArrayIndex, 1, `<span style = "color: rgb(71, 255, 65)">${wordArray[onTypedSubArrayIndex]}</span>`);
-
-  }else{
+    wordArray.splice(
+      onTypedSubArrayIndex,
+      1,
+      `<span style = "color: rgb(71, 255, 65)">${wordArray[onTypedSubArrayIndex]}</span>`
+    );
+  } else {
     wrongWords++;
     letter_penalty += wordArray[onTypedSubArrayIndex].length;
     console.log(`letter penalty: ${letter_penalty}`);
     console.log("replace word red");
-    wordArray.splice(onTypedSubArrayIndex, 1, `<span style = "color: red">${wordArray[onTypedSubArrayIndex]}</span>`);
+    wordArray.splice(
+      onTypedSubArrayIndex,
+      1,
+      `<span style = "color: red">${wordArray[onTypedSubArrayIndex]}</span>`
+    );
   }
 }
 
 //=====================================================
 
-function extract(str){
-  let middle = str.slice(
-    str.indexOf(">") + 1,
-    str.lastIndexOf("<"),
-  );
+function extract(str) {
+  let middle = str.slice(str.indexOf(">") + 1, str.lastIndexOf("<"));
 
-  let colour = str.slice(
-    str.indexOf(":") + 1,
-    str.lastIndexOf('"'),
-  );
-  
-  if(colour == " red"){
+  let colour = str.slice(str.indexOf(":") + 1, str.lastIndexOf('"'));
+
+  if (colour == " red") {
     wrongWords--;
     letter_penalty -= middle.length;
   }
@@ -274,11 +378,11 @@ function extract(str){
 
 //=====================================================
 
-function getTime(){
-  if(startTime){
+function getTime() {
+  if (startTime) {
     seconds--;
     timer.innerHTML = seconds;
-    if (seconds == 0){
+    if (seconds == 0) {
       startTime = false;
       typeField.value = "times up!";
       typeField.disabled = true;
@@ -289,21 +393,21 @@ setInterval(getTime, 1000);
 
 //=====================================================
 
-function ten_sec(){
+function ten_sec() {
   seconds = 11;
   calc_seconds = 10;
   timer.innerHTML = 10;
   restart();
 }
 
-function thirty_sec(){
+function thirty_sec() {
   seconds = 31;
   calc_seconds = 30;
   timer.innerHTML = 30;
   restart();
 }
 
-function sixty_sec(){
+function sixty_sec() {
   seconds = 61;
   calc_seconds = 60;
   timer.innerHTML = 60;
@@ -312,13 +416,15 @@ function sixty_sec(){
 
 //=====================================================
 
-function start(){
+function start() {
   generateWords();
   generateWords();
 }
 
 start();
 
-function showVar(){
-  console.log(`wordsArrayIndex: ${wordsArrayIndex} \t onTypedSubArrayIndex: ${onTypedSubArrayIndex} \t letters: ${letters} \t wrongWords: ${wrongWords} \n onArray: ${onArray}`);
+function showVar() {
+  console.log(
+    `wordsArrayIndex: ${wordsArrayIndex} \t onTypedSubArrayIndex: ${onTypedSubArrayIndex} \t letters: ${letters} \t wrongWords: ${wrongWords} \n onArray: ${onArray}`
+  );
 }
